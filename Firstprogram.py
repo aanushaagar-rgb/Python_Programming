f0e332f01490aaad3880def2298fa9a0341d17cf
aanushaagar-rgb <aanushaagar@gmail.com>
2026-01-21 13:18:21 +0530

    test

---
+++ Firstprogram.py
@@ -0,0 +1,39 @@
+#Arithmetic Operators
+a = 10
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)#Modulo(Remainder)
+print(a ** b)#For calculating power (a^b)
+
+#Relational Operators/Comparison Operators
+#Always gives a boolean value(True or False)
+a = 10
+b = 10
+print(a == b)#equal too
+print(a != b)#not equal too
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+#Arithmetic Operators
+num = 10
+num+= 5#Example
+print("Num is:", num)#(-=, *=, /=, %=, **= )
+
+#Logical Operators(not , and ,or)
+a = 40
+b = 50
+print(not (a>b))#if condition is false & we write not before it, returns true
+print(not (a<b))#if condition is true & we write not before it, returns false
+
+value1 = True
+value2 = False
+print("AND operator:", value1 and value2)
+print("OR operator:", value1 or value2)
+
+
